a72d9cec430740ccdd5d73764a6bdfd24d8e082c
Karl Ehatäht <karlehataht@gmail.com>
2016-12-01 00:43:22 +0200

    Update samples (ttHToNonbb -> ttHJetToNonbb)

--- python/runMEM_3l1tau_2016_cfg.py
+++ python/runMEM_3l1tau_2016_cfg.py
@@ -9,7 +9,7 @@ if useGen:
   baseDir = os.path.join(baseDir, "gen")
 
 process.fwliteInput = cms.PSet(
-  fileNames = cms.vstring(os.path.join(baseDir, "out_3l_1tau_ttHToNonbb_M125_Tight_OS_central.root")),
+  fileNames = cms.vstring(os.path.join(baseDir, "out_3l_1tau_ttHJetToNonbb_M125_Tight_OS_central.root")),
   # or use:
   # out_3l_1tau_TTZToLLNuNu_Tight_OS_central.root
   maxEvents = cms.int32(3),        # test with only three events
